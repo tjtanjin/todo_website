3f8a84e24dacac617eea8c36be769d9c72ca2188
tjtanjin <cjtanjin@gmail.com>
2019-12-12 03:43:20 +0800

    Updated homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from "../img/logo.png";
+import { Logo } from '../components/AuthForms';
 
 function Home(props) {
-  return <div>Home Page</div>;
+  return (
+  	<div className="home-inner">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>Todo_Manager</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/login"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/signup"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Logo src={logoImg} />
+    </div>
+  )
 }
 
 export default Home;
