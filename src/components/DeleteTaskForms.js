fb12e61d49b6de4161753961e15220f31091b01f
tjtanjin <cjtanjin@gmail.com>
2019-12-14 02:41:26 +0800

    Modals from creating and deleting tasks added to components.

---
+++ src/components/DeleteTaskForms.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from 'axios';
+import { Form, Error, Success } from "../components/AuthForms";
+import { decode } from 'jsonwebtoken'
+import { useAuth } from "../context/auth";
+import { Sleep } from "../components/Utils"
+
+function DeleteTask(data) {
+  function delTask() {
+    const token = JSON.parse(localStorage.getItem('tokens')).auth_token;
+    const user_id = decode(token).user_id;
+    axios.delete("http://localhost:3000/api/v1/users/" + user_id + "/tasks/" + data.id, {
+      headers: { Authorization: token }
+    }).then(result => {
+      if (result.status === 200) {
+        window.location.assign("/tasks");
+      } else {
+
+      }
+    }).catch(e => {
+      alert(e)
+    });
+  }
+
+  return (
+    <div className="auth-inner">
+      <p className="prompt"> Are you sure you want to delete the task <span className="trackedtask">{data.job_name}</span>?</p>
+      <br/>
+      <button className="btn btn-primary btn-block" onClick={e => delTask()}>Delete</button>
+    </div>
+  );
+}
+
+export default DeleteTask
---
+++ src/components/NewTaskForms.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from 'axios';
+import { Form, Error, Success } from "../components/AuthForms";
+import { decode } from 'jsonwebtoken'
+import { useAuth } from "../context/auth";
+import { Sleep } from "../components/Utils"
+
+function NewTask() {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [job_name, setJobName] = useState("");
+  const [job_desc, setJobDesc] = useState("");
+  const [category, setCategory] = useState("");
+  const [tag, setTag] = useState("");
+  const [due, setDue] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+  const { setAuthTokens } = useAuth();
+  let history = useHistory();
+
+  function validateInput() {
+    if (job_name !== "" && job_desc !== "" && category !== "") {
+      postNewTask();
+    } else {
+      return false;
+    }
+  }
+
+  function postNewTask() {
+    const token = JSON.parse(localStorage.getItem('tokens')).auth_token;
+    const user_id = decode(token).user_id;
+    axios.post("http://localhost:3000/api/v1/users/" + user_id + "/tasks", {
+      job_name,
+      job_desc,
+      category,
+      tag,
+      due,
+      user_id
+    }, {
+      headers: { Authorization: token }
+    }).then(result => {
+      if (result.status === 200) {
+        setIsSuccess(true);
+        Sleep(1000).then(() => {
+          window.location.assign("/tasks");
+        })
+      } else {
+        setIsError(true);
+      }
+    }).catch(e => {
+      setIsError(true);
+    });
+  }
+
+  return (
+    <div className="auth-inner">
+      <form>
+        <div className="form-group">
+          <label>Task Name</label>
+          <input
+            type="job_name"
+            value={job_name}
+            className="form-control" 
+            onChange={e => {
+              setJobName(e.target.value);
+            }}
+            placeholder="Enter task name"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Task Description</label>
+          <input
+            type="job_desc"
+            value={job_desc}
+            className="form-control" 
+            onChange={e => {
+              setJobDesc(e.target.value);
+            }}
+            placeholder="Enter description"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Category</label>
+          <input
+            type="category"
+            value={category}
+            className="form-control" 
+            onChange={e => {
+              setCategory(e.target.value);
+            }}
+            placeholder="Enter category"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Tag</label>
+          <input
+            type="tag"
+            value={tag}
+            className="form-control" 
+            onChange={e => {
+              setTag(e.target.value);
+            }}
+            placeholder="Choose a tag"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Deadline</label>
+          <input
+            type="due"
+            value={due}
+            className="form-control" 
+            onChange={e => {
+              setDue(e.target.value);
+            }}
+            placeholder="Enter deadline"
+          />
+        </div>
+
+        <button id="submitButton" type="submit" className="btn btn-primary btn-block" onClick={validateInput}>Create</button>
+        <button type="button" className="btn btn-primary btn-block" onClick={event => window.location.href='/tasks'}>Back</button>
+        <br/>
+        { isSuccess &&<Success>Task created!</Success> }
+        { isError &&<Error>Please check the format of your information.</Error> }
+      </form>
+    </div>
+  );
+}
+
+export default NewTask;
