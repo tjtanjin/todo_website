c62f9286d61276486ddf701ce7bddfc3f1d520c5
tjtanjin <cjtanjin@gmail.com>
2019-12-16 00:37:30 +0700

    Now decodes token to check for validity of request.

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,15 +1,26 @@
 import React from "react";
+import axios from 'axios';
+import { decode } from 'jsonwebtoken'
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "./context/auth";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const { authTokens } = useAuth();
 
+  function verify() {
+    const token = JSON.parse(localStorage.getItem('tokens')).auth_token;
+    if (decode(token) === null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   return (
     <Route
       {...rest}
       render={props =>
-        JSON.parse(localStorage.getItem('tokens')).auth_token !== undefined ? (
+        verify() ? (
           <Component {...props} />
         ) : (
           <Redirect 
