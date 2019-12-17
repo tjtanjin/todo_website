ca2c8357e82a2a7678db93d832b6b7f2c696335f
tjtanjin <cjtanjin@gmail.com>
2019-12-17 17:59:39 +0800

    Fixed minor bug with verifying authentication.

--- src/components/VerifyAuth.js
+++ src/components/VerifyAuth.js
@@ -1,7 +1,12 @@
+import React from "react";
 import { decode } from 'jsonwebtoken'
 
 function VerifyAuth() {
-  const token = JSON.parse(localStorage.getItem('tokens')).auth_token;
+  const check_exist_token = JSON.parse(localStorage.getItem('tokens'));
+  if (check_exist_token === null) {
+  	return false;
+  }
+  const token = check_exist_token.auth_token;
   if (decode(token) === null) {
     return false;
   } else {
