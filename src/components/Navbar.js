59eaef7407718f2e679bc6ede572448d0606b66d
tjtanjin <cjtanjin@gmail.com>
2019-12-19 08:12:54 +0800

    Added link on navbar to go to tasks.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,11 +14,15 @@ function Navbar() {
   let word1 = "Login"
   let link2 = "/signup"
   let word2 = "Sign Up"
+  let link3 = ""
+  let word3 = ""
   if (VerifyAuth()) {
     link1 = "/profile"
     word1 = "My Profile"
     link2 = "/"
     word2 = "Logout"
+    link3 = "/tasks"
+    word3 = "My Tasks"
   } else {}
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
@@ -26,6 +30,9 @@ function Navbar() {
         <Link className="navbar-brand nav-link navbar-nav text-uppercase" to={"/"}>Todo_Manager</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav text-uppercase ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link nav-text" to={link3}>{word3}</Link>
+            </li>
             <li className="nav-item">
               <Link className="nav-link nav-text" to={link1}>{word1}</Link>
             </li>
