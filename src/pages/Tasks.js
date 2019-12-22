cd678535d3c9151f0e451e3956477df1a2615f80
tjtanjin <cjtanjin@gmail.com>
2019-12-22 15:50:13 +0800

    Refactored code to improve authentication and include additional user information.

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -55,7 +55,7 @@ function Tasks(props) {
   }
 
   function getTasks() {
-    const token = JSON.parse(localStorage.getItem('tokens')).auth_token;
+    const token = JSON.parse(localStorage.getItem('todo_data')).auth_token;
     const user_id = decode(token).user_id;
     axios.get(process.env.REACT_APP_API_LINK + "/api/v1/users/" + user_id + "/tasks", {
       headers: { Authorization: token }
