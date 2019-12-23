9d53c75b20fd9bc1b3d8bd89d65d1fc54dc0f638
tjtanjin <cjtanjin@gmail.com>
2019-12-23 20:43:48 +0800

    Added formatDate function to transform date into human readable format.

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,5 +1,10 @@
-function Sleep(ms) {
+function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-export { Sleep };
+function formatDate(string){
+  var options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  return new Date(string).toLocaleDateString([],options);
+}
+
+export { sleep, formatDate };
