f26a2a2f68803fc2c08160f35d87a2fff6f500f0
tjtanjin <cjtanjin@gmail.com>
2019-12-24 00:15:29 +0800

    Added profile page for users.

---
+++ src/components/EditUserForms.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Error, Success } from "../components/AuthForms";
+import { decode } from 'jsonwebtoken'
+
+function EditUser(data) {
+  const onCloseModal = data.onCloseModal
+  const getSelf = data.getSelf;
+  data = data.user
+  const [apiResult, setApiResult] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [name, setUsername] = useState(data.name);
+  const [email, setEmail] = useState(data.email);
+
+  function putEditUser() {
+    const token = JSON.parse(localStorage.getItem('todo_data')).auth_token;
+    const user_id = decode(token).user_id;
+    axios.put(process.env.REACT_APP_API_LINK + "/users/" + user_id, { "user": {
+      name,
+      email
+    }}, {
+      headers: { Authorization: token }
+    }).then(result => {
+      if (result.status === 200) {
+        setIsSuccess(true)
+        getSelf();
+        onCloseModal()
+      } else {
+        setIsError(true)
+      }
+    }).catch(e => {
+      setApiResult(e.response.data.error);
+      setIsError(true);
+    });
+  }
+
+  useEffect(() => {
+    const handleEnter = (event) => {
+      if (event.keyCode === 13) {
+        document.getElementById("submitButton").click()
+      }
+    };
+    window.addEventListener('keydown', handleEnter);
+
+    return () => {
+      window.removeEventListener('keydown', handleEnter);
+    };
+  }, []);
+
+  return (
+    <div className="auth-inner">
+      <form>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            type="name"
+            defaultValue={data.name}
+            className="form-control" 
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+            placeholder=""
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            defaultValue={data.email}
+            className="form-control" 
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+            placeholder=""
+          />
+        </div>
+
+        <button id="submitButton" type="button" className="btn btn-dark btn-block" onClick={putEditUser}>Update</button>
+        <button type="button" className="btn btn-dark btn-block" onClick={onCloseModal}>Back</button>
+        <br/>
+        { isSuccess &&<Success>Task updated!</Success> }
+        { isError &&<Error>{apiResult}</Error> }
+      </form>
+    </div>
+  );
+}
+
+export default EditUser
---
+++ src/pages/Profile.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from 'axios';
+import EditUser from "../components/EditUserForms";
+import { Modal } from 'react-bootstrap'
+import { Form, Error } from "../components/AuthForms";
+import { Loading } from "../components/Loading";
+import { useAuth } from "../context/auth";
+import { decode } from 'jsonwebtoken';
+import { Navbar } from "../components/Navbar";
+import { formatDate } from "../components/Utils";
+
+function Profile(props) {
+  const [isError, setIsError] = useState(false);
+  const [showEditUser, setEditUserShow] = useState(false);
+  const [userid, setUserID] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [createdate, setCreateDate] = useState("");
+  const [modifydate, setModifyDate] = useState("");
+  const [password, setPassword] = useState("");
+  const [trackedUser, setTrackedUser] = useState("");
+
+  const handleEditUserClose = () => setEditUserShow(false);
+  const handleEditUserShow = () => setEditUserShow(true);
+
+  function getSelf() {
+    const token = JSON.parse(localStorage.getItem('todo_data')).auth_token;
+    const user_id = decode(token).user_id;
+    axios.get(process.env.REACT_APP_API_LINK + "/users/" + user_id, {
+      headers: { Authorization: token }
+    }).then(result => {
+      if (result.status === 200) {
+        setTrackedUser(result.data);
+        setUserID(result.data.id);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setRole(result.data.role);
+        setCreateDate(formatDate(result.data.created_at));
+        setModifyDate(formatDate(result.data.updated_at));
+      } else {
+
+      }
+    }).catch(e => {
+
+    });
+  }
+
+  useEffect(() => {
+    getSelf();
+  }, []);
+
+  return (
+    <div className="auth-inner">
+      <Navbar></Navbar>
+      
+      <h3>{name}'s Profile</h3>
+      <br/>
+      <div class="tab-content profile-tab" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+          <div class="row">
+            <div class="col-md-6">
+              <label>User ID</label>
+            </div>
+            <div class="col-md-6">
+            <p>{userid}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+                <label>Name</label>
+            </div>
+            <div class="col-md-6">
+                <p>{name}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+                <label>Email</label>
+            </div>
+            <div class="col-md-6">
+                <p>{email}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+                <label>Role</label>
+            </div>
+            <div class="col-md-6">
+                <p>{role}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+                <label>Created at</label>
+            </div>
+            <div class="col-md-6">
+                <p>{createdate}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+                <label>Last modified</label>
+            </div>
+            <div class="col-md-6">
+                <p>{modifydate}</p>
+            </div>
+          </div>
+          <button id="submitButton" className="btn btn-dark btn-block" onClick={() => {handleEditUserShow()}}>Edit</button>
+        </div>
+      </div>
+
+      <Modal show={showEditUser} onHide={handleEditUserClose}>
+        <Modal.Header className="modal_header_bg">
+          <Modal.Title>Edit Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><EditUser user={trackedUser} onCloseModal={handleEditUserClose} getSelf={getSelf}></EditUser></Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default Profile;
