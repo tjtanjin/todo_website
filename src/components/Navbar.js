142d5766ef2fe8d14b9a15a59e6b9517e304ed90
tjtanjin <cjtanjin@gmail.com>
2019-12-26 03:36:43 +0800

    Added highlight on navigation bar for current page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { VerifyAuth } from "../components/VerifyAuth";
 import { VerifyAdmin } from "../components/VerifyAdmin";
 import { useAuth } from "../context/auth"
@@ -34,20 +34,20 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
       <div className="container">
-        <Link className="navbar-brand nav-link navbar-nav text-uppercase" to={"/"}>Todo_Manager</Link>
+        <NavLink className="navbar-brand nav-link navbar-nav text-uppercase" to={"/"}>Todo_Manager</NavLink>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav text-uppercase ml-auto">
             <li className="nav-item">
-              <Link className="nav-link nav-text" to={link4}>{word4}</Link>
+              <NavLink activeClassName="nav-selected" className="nav-link nav-text" to={link4}>{word4}</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link nav-text" to={link3}>{word3}</Link>
+              <NavLink activeClassName="nav-selected" className="nav-link nav-text" to={link3}>{word3}</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link nav-text" to={link1}>{word1}</Link>
+              <NavLink activeClassName="nav-selected" className="nav-link nav-text" to={link1}>{word1}</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link nav-text" to={link2} onClick={logOut}>{word2}</Link>
+              <NavLink className="nav-link nav-text" to={link2} onClick={logOut}>{word2}</NavLink>
             </li>
           </ul>
         </div>
